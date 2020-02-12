bf4bc829f49ba84a3d38df1f6712a0c61bd2f04c
breeku <bregfi@hotmail.com>
2020-02-12 13:34:07 +0200

    class components to functional

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,33 @@
-import React from 'react';
-import { graphql, navigate, withPrefix } from 'gatsby'
-import { getUserLangKey } from 'ptz-i18n';
+import React from "react"
+import { graphql, navigate, withPrefix } from "gatsby"
+import { getUserLangKey } from "ptz-i18n"
 
-class RedirectIndex extends React.PureComponent {
-  constructor(args) {
-    super(args);
-
+const RedirectIndex = args => {
     // Skip build, Browsers only
-    if (typeof window !== 'undefined') {
-      const { langs, defaultLangKey } = args.data.site.siteMetadata.languages;
-      const langKey = getUserLangKey(langs, defaultLangKey);
-      const homeUrl = withPrefix(`/${langKey}/`);
+    if (typeof window !== "undefined") {
+        const { langs, defaultLangKey } = args.data.site.siteMetadata.languages
+        const langKey = getUserLangKey(langs, defaultLangKey)
+        const homeUrl = withPrefix(`/${langKey}/`)
 
-      navigate(homeUrl);
+        navigate(homeUrl)
     }
-  }
 
-  render() {
     // It's recommended to add your SEO solution in here for bots
     // eg. https://github.com/ahimsayogajp/ahimsayoga-gatsby/blob/master/src/pages/index.js#L22
-    return (<div />);
-  }
+    return <div />
 }
 
-export default RedirectIndex;
+export default RedirectIndex
 
 export const pageQuery = graphql`
-  query IndexQuery {
-    site{
-      siteMetadata{
-        languages {
-          defaultLangKey
-          langs
+    query IndexQuery {
+        site {
+            siteMetadata {
+                languages {
+                    defaultLangKey
+                    langs
+                }
+            }
         }
-      }
     }
-  }
-`;
+`
